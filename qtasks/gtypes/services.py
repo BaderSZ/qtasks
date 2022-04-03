3a4018ab5ece656f79e04b0b3b29b451e15b2190
Bader Zaidan <bader@zaidan.pw>
2022-04-04 00:09:50 +0200

    Fix add function

--- qtasks/gtypes/services.py
+++ qtasks/gtypes/services.py
@@ -16,9 +16,16 @@ class TaskService(skeleton.ServiceType):
         super().__init__(service)
         self._service = service.tasks()
 
-    def add(self, task: Task):
-        """Add task to queue. Run execute() after this."""
-        super().add(task)
+    def add(self, task: Task, tasklist: TaskList = None):
+        """Add task to queue. Run execute() after this."""       
+        if tasklist is None:
+            self._queue_request(self._service.insert(tasklist="@default", body=task.dump()))    
+        else:
+            self._queue_request(self._service.insert(tasklist=tasklist.get_id(), body=task.dump()))
+
+    def get(self, tasklist: TaskList, show_completed: bool = False):
+        """Get list of tasks in a tasklist."""
+        super().get(tasklist=tasklist, show_completed=show_completed)
 
     def clear(self, tasklist: TaskList) -> None:
         """Clear tasklist and add request to queue."""
@@ -36,10 +43,15 @@ class TaskListService(skeleton.ServiceType):
         """Create a tasklist service, given an existing ServiceManager resource.."""
         super().__init__(service)
         self._service = service.tasklists()
+        self.default_id = "@default"
 
-    def add(self, tasklist: TaskList):
+    def add(self, tasklist: TaskList) -> None:
         """Add tasklist to queue. Run execute() after this."""
         super().add(tasklist)
+
+    def get(self) -> None:
+        """Get list of TaskLists."""
+        super().get()
 
     def delete(self, tasklist: TaskList) -> None:
         """Delete Tasklist and add request to queue."""

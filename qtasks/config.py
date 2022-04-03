8bebbb2b2e2a96faca86d7178406c765bf005212
Bader Zaidan <bader@zaidan.pw>
2022-04-03 19:30:07 +0200

    Add initial configs and servicemanager class

---
+++ qtasks/config.py
@@ -0,0 +1,9 @@
+"""Google API configuration variables."""
+
+API_SERVICE_NAME = "tasks"
+API_VERSION = "v1"
+
+SCOPES = ["https://www.googleapis.com/auth/tasks",
+          "https://www.googleapis.com/auth/tasks.readonly"]
+
+DISCOVERY_URL = "https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest"
---
+++ qtasks/servicemanager.py
@@ -0,0 +1,80 @@
+"""Service manager class. Contains the necessary functions to create and build api discovery \
+    service."""
+
+try:
+    from os import path
+    # from queue import Queue
+    import logging
+
+    # Auth libs
+    from google.auth.transport.requests import Request
+    from google.auth.exceptions import (OAuthError, ReauthFailError, RefreshError, 
+                                        TransportError)  # , UserAccessTokenError)
+
+    from google.oauth2.credentials import Credentials
+    from google_auth_oauthlib.flow import InstalledAppFlow
+
+    # API libs
+    from googleapiclient.discovery import build
+    from googleapiclient.discovery import Resource
+
+    # Local
+    from keys import keys
+    import config
+
+    from gtypes.exceptions import AuthenticationError
+
+
+except ImportError as importexp:
+    logging.critical("Import Exception: Could not import a library!\n %s", importexp)
+    raise
+
+
+class ServiceManager:
+    """Google Services manager object. Creates and manages keys and services."""
+
+    def __init__(self) -> None:
+        """Initiate an empty object."""
+        self.credentials = None
+        self.service = None
+
+    def create_key(self, token_file: str, flow_cli: bool) -> None:
+        """Create a client key given a token file."""
+        # TODO: Other authentication methods
+        try:
+            flow = InstalledAppFlow.from_client_secrets_file(keys.CLIENT_SECRETS_FILE, config.SCOPES)
+            if flow_cli is False:
+                self.credentials = flow.run_console()  # shows URL in CLI
+            else:
+                self.credentials = flow.run_local_server()  # Opens local tab
+        except ValueError as verr:
+            logging.exception("flow could not create credentials! Check docs (and comment) %s", verr)
+            raise
+        with open(token_file, 'w') as token:
+            token.write(self.credentials.to_json())
+
+    def create(self, token_file: str, flow_cli: bool = False) -> Resource:
+        """Create a Resource for interacting with the Google Tasks API."""
+        # Get token
+        if path.exists(token_file):
+            logging.info("Found token %s. Using...", token_file)
+            try:
+                self.credentials = Credentials.from_authorized_user_file(token_file, config.SCOPES)
+                if not self.credentials or not self.credentials.valid:
+                    if (self.credentials and self.credentials.expired and self.credentials.refresh_token):
+                        logging.info("Refreshing expired token")
+                        self.credentials.refresh(Request())
+                    else:
+                        self.create_key(token_file, flow_cli)
+            except (OAuthError, ReauthFailError, RefreshError, TransportError) as authexp:
+                logging.exception("Could not create credentials!\n%s", authexp)
+                logging.exception("Try again?")
+                raise AuthenticationError("Could not authenticate: ") from authexp
+            except OSError as exp:
+                logging.critical("Could not open file! %s", exp)
+                raise
+        else:
+            logging.info("No token file found. Creating...")
+            self.create_key(token_file, flow_cli)
+
+        self.service = build(config.API_SERVICE_NAME, config.API_VERSION, credentials=self.credentials)

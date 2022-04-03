5e3b0b99e066deea8f17259de363a1369fdbde3a
Bader Zaidan <bader@zaidan.pw>
2022-04-03 11:58:20 +0200

    Add init and empty tests

--- tests/test_qtasks.py
+++ tests/test_qtasks.py
@@ -1,5 +1,8 @@
+"""Run tests from all modules."""
+
 from qtasks import __version__
 
 
 def test_version():
+    """Make sure the correct version number is being tested."""
     assert __version__ == '0.1.0'

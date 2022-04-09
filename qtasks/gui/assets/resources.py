57090b9d0817dfa69a305d4285e5b1713e79a043
Bader Zaidan <bader@zaidan.pw>
2022-04-09 21:44:56 +0200

    Add graphical assets and resources files

---
+++ qtasks/gui/assets/resources.py
@@ -0,0 +1,135 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.2.4
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x06\x00\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22UTF\
+-8\x22 standalone=\x22\
+no\x22?>\x0a<svg\x0a   ve\
+rsion=\x221.0\x22\x0a   w\
+idth=\x22120.000000\
+pt\x22\x0a   height=\x221\
+20.000000pt\x22\x0a   \
+viewBox=\x220 0 120\
+.000000 120.0000\
+00\x22\x0a   preserveA\
+spectRatio=\x22xMid\
+YMid meet\x22\x0a   id\
+=\x22svg8\x22\x0a   sodip\
+odi:docname=\x22tri\
+angle.svg\x22\x0a   in\
+kscape:version=\x22\
+1.1.2 (0a00cf533\
+9, 2022-02-04)\x22\x0a\
+   xmlns:inkscap\
+e=\x22http://www.in\
+kscape.org/names\
+paces/inkscape\x22\x0a\
+   xmlns:sodipod\
+i=\x22http://sodipo\
+di.sourceforge.n\
+et/DTD/sodipodi-\
+0.dtd\x22\x0a   xmlns=\
+\x22http://www.w3.o\
+rg/2000/svg\x22\x0a   \
+xmlns:svg=\x22http:\
+//www.w3.org/200\
+0/svg\x22>\x0a  <defs\x0a\
+     id=\x22defs12\x22\
+ />\x0a  <sodipodi:\
+namedview\x0a     i\
+d=\x22namedview10\x22\x0a\
+     pagecolor=\x22\
+#ffffff\x22\x0a     bo\
+rdercolor=\x22#6666\
+66\x22\x0a     bordero\
+pacity=\x221.0\x22\x0a   \
+  inkscape:pages\
+hadow=\x222\x22\x0a     i\
+nkscape:pageopac\
+ity=\x220.0\x22\x0a     i\
+nkscape:pagechec\
+kerboard=\x220\x22\x0a   \
+  inkscape:docum\
+ent-units=\x22pt\x22\x0a \
+    showgrid=\x22fa\
+lse\x22\x0a     inksca\
+pe:zoom=\x225.44375\
+\x22\x0a     inkscape:\
+cx=\x2243.811711\x22\x0a \
+    inkscape:cy=\
+\x2280\x22\x0a     inksca\
+pe:window-width=\
+\x221918\x22\x0a     inks\
+cape:window-heig\
+ht=\x221058\x22\x0a     i\
+nkscape:window-x\
+=\x220\x22\x0a     inksca\
+pe:window-y=\x2220\x22\
+\x0a     inkscape:w\
+indow-maximized=\
+\x221\x22\x0a     inkscap\
+e:current-layer=\
+\x22g6\x22 />\x0a  <metad\
+ata\x0a     id=\x22met\
+adata2\x22>\x0aCreated\
+ by potrace 1.16\
+, written by Pet\
+er Selinger 2001\
+-2019\x0a</metadata\
+>\x0a  <g\x0a     tran\
+sform=\x22translate\
+(0.000000,120.00\
+0000) scale(0.10\
+0000,-0.100000)\x22\
+\x0a     fill=\x22#000\
+000\x22\x0a     stroke\
+=\x22none\x22\x0a     id=\
+\x22g6\x22>\x0a    <path\x0a\
+       d=\x22M12 10\
+56 c4 -20 581 -1\
+016 588 -1016 7 \
+0 584 996 588 10\
+16 3 12 -91 14 -\
+588 14 -497 0 -5\
+91 -2 -588 -14z\x22\
+\x0a       id=\x22path\
+4\x22\x0a       style=\
+\x22fill:#5f6368;fi\
+ll-opacity:1\x22 />\
+\x0a  </g>\x0a</svg>\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x06\
+\x06\x8a\x9c\xb3\
+\x00a\
+\x00s\x00s\x00e\x00t\x00s\
+\x00\x0c\
+\x05Y\xaaG\
+\x00t\
+\x00r\x00i\x00a\x00n\x00g\x00l\x00e\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x12\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x80\x0fI\x8e\xc9\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()

a34ab47bdd870fa34d2405cc796314a94af0bd33
Bader Zaidan <bader@zaidan.pw>
2022-04-09 21:48:54 +0200

    remove double calls from main file

--- qtasks/__main__.py
+++ qtasks/__main__.py
@@ -1,31 +1,17 @@
 #!/bin/env python
-#!/bin/env python
 
-"""Main function of the program part."""
+"""Main function: start gui."""
 
 # TODO
 
-# import sys
 import logging
 
-from qtasks.manager.servicemanager import ServiceManager
-
-from qtasks.gtypes.services import TaskListService, TaskService
-
-from qtasks.gui.mainwindow import startGui
-# from qtasks.gtypes.task import Task
-# from qtasks.gtypes.tasklist import TaskList
-
-from qtasks.keys import keys
+from qtasks.gui.mainwindow import start_gui
 
 
 # levels: Options: DEBUG, INFO, WARNING, ERROR, CRITICAL
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
+
 if __name__ == "__main__":
-    servicemanager = ServiceManager()
-    servicemanager.create(keys.TOKENFILE)
-    taskservice = TaskService(servicemanager=servicemanager)
-    tasklistservice = TaskListService(servicemanager=servicemanager)
-    # sys.exit()
-    startGui()
+    start_gui()
